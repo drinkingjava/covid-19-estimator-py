fde31bc46b2d9a429ca2146a3c30cd7c9681aba7
algerchen <algerchen@umantec.net>
2020-04-11 22:54:12 +0200

    Calculations for currentlyInfected and infectionsByRequestedTime added

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,59 @@
+import json
+
+stats = {
+    'region': {
+        'name': 'Africa',
+        'avgAge': 19.7,
+        'avgDailyIncomeInUSD': 5,
+        'avgDailyIncomePopulation': 0.71
+    },
+    'periodType': 'days',
+    'timeToElapse': 58,
+    'reportedCases': 674,
+    'population': 66622705,
+    'totalHospitalBeds': 1380614
+}
+
+
+def requestedTimeFactorCalculator(periodType, timeToElapse):
+    if periodType == 'days':
+        days = timeToElapse
+        return 2 ^ int(days/3)
+    elif periodType == 'months':
+        days = timeToElapse * 30
+        return 2 ^ int(days/3)
+    elif periodType == 'years':
+        days = timeToElapse * 12 * 30
+        return 2 ^ int(days/3)
+    else:
+        raise Exception(
+            'Period should be days, months or years.'
+            '{} was given'.format(periodType))
+
+
+def impactCalculator(reportedCases, requestedTimeFactor):
+    currentlyInfected = reportedCases * 10
+    infectionsByRequestedTime = currentlyInfected * requestedTimeFactor
+    return dict(currentlyInfected=currentlyInfected,
+                infectionsByRequestedTime=infectionsByRequestedTime)
+
+
+def severeImpactCalculator(reportedCases, requestedTimeFactor):
+    currentlyInfected = reportedCases * 50
+    infectionsByRequestedTime = currentlyInfected * requestedTimeFactor
+    return dict(currentlyInfected=currentlyInfected,
+                infectionsByRequestedTime=infectionsByRequestedTime)
+
+
 def estimator(data):
-  return data
+    reportedCases = data['reportedCases']
+    timeFactor = requestedTimeFactorCalculator(
+        data['periodType'], data['timeToElapse'])
+    data = dict(data=data,
+                impact=impactCalculator(reportedCases, timeFactor),
+                severeImpact=severeImpactCalculator(reportedCases, timeFactor))
+    return data
+
+
+if __name__ == '__main__':
+    print(json.dumps(estimator(stats)))
